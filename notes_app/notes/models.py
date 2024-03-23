45b84e87ce0ed13681089fe11efe6900671c813f
daryna <darynadobro@gmail.com>
2024-03-23 16:51:01 +0100

    add related name field for note

--- notes_app/notes/models.py
+++ notes_app/notes/models.py
@@ -14,7 +14,7 @@ class Note(models.Model):
     title = models.CharField(max_length=225)
     text = models.TextField()
     reminder = models.DateTimeField(blank=True, null=True)
-    categories = models.ManyToManyField(Category)
+    categories = models.ManyToManyField(Category, related_name="notes")
 
     def __str__(self):
         return f"Note: {self.title}"
